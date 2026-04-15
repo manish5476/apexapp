2640390e66ebcd752d8c38d3d11398975448b3f5
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-04-15 10:32:24 +0530

    Update environment.ts

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,18 +1,28 @@
 // src/constants/environment.ts
+
 export const environment = {
   production: false,
-  apiUrl: process.env.EXPO_PUBLIC_API_URL ,//|| 'http://10.155.124.42:5000/api',
-  socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL //|| 'http://10.155.124.42:5000',
-  // apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://apex-1ed5.onrender.com/api',
-  // socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL || 'https://apex-1ed5.onrender.com',
+  
+  // 1. Tries to read the environment variable injected by eas.json or .env
+  // 2. If it fails or is undefined, it safely falls back to your Render server
+  apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://apex-1ed5.onrender.com/api',
+  socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL || 'https://apex-1ed5.onrender.com',
 };
-
+// // src/constants/environment.ts
 // export const environment = {
-//   production: true,
-//   // apiUrl: 'http://localhost:4002/api'
-//   // apiUrl: 'https://shivamelectronicsbackend.onrender.com/api'
-//   // apiUrl: 'https://shivamelectronicsbackendprod.onrender.com/api'
-//   apiUrl: 'https://apex-1ed5.onrender.com/api',
-//   socketUrl:'https://apex-1ed5.onrender.com',
-
+//   production: false,
+//   apiUrl: process.env.EXPO_PUBLIC_API_URL ,//|| 'http://10.155.124.42:5000/api',
+//   socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL //|| 'http://10.155.124.42:5000',
+//   // apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://apex-1ed5.onrender.com/api',
+//   // socketUrl: process.env.EXPO_PUBLIC_SOCKET_URL || 'https://apex-1ed5.onrender.com',
 // };
+
+// // export const environment = {
+// //   production: true,
+// //   // apiUrl: 'http://localhost:4002/api'
+// //   // apiUrl: 'https://shivamelectronicsbackend.onrender.com/api'
+// //   // apiUrl: 'https://shivamelectronicsbackendprod.onrender.com/api'
+// //   apiUrl: 'https://apex-1ed5.onrender.com/api',
+// //   socketUrl:'https://apex-1ed5.onrender.com',
+
+// // };
